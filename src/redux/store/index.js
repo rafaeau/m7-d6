59aed293e72ab9b8877dd11772873a4a1eb75c5d
Rafael Ferreira <rafaelnevesf@gmail.com>
2022-01-31 16:15:16 +0100

    Redux store surviving a browser refresh with redux-persist

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,23 +2,32 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import jobsReducer from '../reducers/jobsReducer'
 import favoritesReducer from '../reducers/favoritesReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const composeThatAlwaysWorks = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const initialState = {
-    jobs: [],
-    favorites: [],
+  jobs: [],
+  favorites: [],
 }
 
 const bigReducer = combineReducers({
-    jobs: jobsReducer,
-    favorites: favoritesReducer,
-  })
+  jobs: jobsReducer,
+  favorites: favoritesReducer,
+})
 
-let configureStore = createStore(
-  bigReducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, bigReducer)
+
+export let configureStore = createStore(
+  persistedReducer,
   initialState,
   composeThatAlwaysWorks(applyMiddleware(thunk))
 )
 
-export default configureStore
+export const persistor = persistStore(configureStore)
